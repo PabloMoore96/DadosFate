730112b13888726b3d441f21e2960c3d875e01c5
Pablo Moore <105167808+PabloMoore96@users.noreply.github.com>
2023-05-12 01:55:27 -0300

    Funcionalidad completa

--- app.js
+++ app.js
@@ -4,24 +4,37 @@
 y la cantidad de caras que se le hayan ingresado al dado */
 function dados_tirar(nCaras, cant){
     
+    var arrayRespuesta = []
+    let resultado = document.getElementById("answer");
+
     for (let i = 0; i < cant; i++) {
 
         let numAleatorio = 1 + Math.floor(Math.random() * nCaras);
         if(numAleatorio === 1){
-            alert (`Resultado dado ${i+1} es "+"`)
+            let resp =`Dado ${i+1}: "+" | `
+            arrayRespuesta.push(resp)
         }else if (numAleatorio === 2){
-            alert (`Resultado dado ${i+1} es "+"`)
+            let resp =`Dado ${i+1}: "+" | `
+            arrayRespuesta.push(resp)
         }else if (numAleatorio === 3){
-            alert (`Resultado dado ${i+1} es "-"`)
+            let resp =`Dado ${i+1}: "-" | `
+            arrayRespuesta.push(resp)
         }else if (numAleatorio === 4){
-            alert (`Resultado dado ${i+1} es "-"`)
+            let resp =`Dado ${i+1}: "-" | `
+            arrayRespuesta.push(resp)
         }else if (numAleatorio === 5){
-            alert (`Resultado dado ${i+1} es " "`)
+            let resp =`Dado ${i+1}: "  " | `
+            arrayRespuesta.push(resp)
         }else if (numAleatorio === 6){
-            alert (`Resultado dado ${i+1} es " "`)
+            let resp =`Dado ${i+1}: "  " | `
+            arrayRespuesta.push(resp)
         }
+
         
     }
+
+    let respuesta = arrayRespuesta.join(" ")
+    resultado.textContent = respuesta
     
 
 }
@@ -34,7 +47,16 @@ function validateForm(e){
     e.preventDefault()
     let cantDados = document.getElementById("dado").value
 
-    console.log(cantDados)
-    dados_tirar(6, cantDados)
+    if (cantDados > 25){
+        let resultado = document.getElementById("answer");
+        resultado.textContent = "Por favor, no vas a tirar más de 25 dados."
+    }else if(cantDados<1){
+        let resultado = document.getElementById("answer");
+        resultado.textContent = "No podés tirar 0 dados o menos."
+    }else{
+        dados_tirar(6, cantDados)
+    }
+    
 
 }
+
